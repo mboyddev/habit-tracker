3b99711f4341789a904031c432d706e4ba5cca13
Michael Boyd <michaelboyddev@gmail.com>
2020-06-02 18:47:41 -0400

    Add login form

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ let boxes = document.querySelectorAll('input[type=checkbox');
 let boxCount = JSON.parse(localStorage.getItem('boxCount'));
 let pastSession = (boxCount > 1);
 
+let loginWindow = document.getElementById('login-window');
+let pwInput = document.getElementById('password');
+let errorMsg = document.getElementById('error-msg');
+let submitBtn = document.getElementById('submit-btn');
+let authorized = sessionStorage.getItem('auth-status');
+if(authorized) {
+    loginWindow.classList.add('invisible');
+    console.log('authorized');
+}
+
+submitBtn.addEventListener('click', () => {
+    let pw = pwInput.value;
+    console.log(pw);
+    pwInput.value = '';
+    if(pw === 'Ebony99!') {
+        loginWindow.classList.add('invisible');
+        sessionStorage.setItem('auth-status', "true");
+    } else {
+        errorMsg.classList.remove('invisible');
+    }
+})
+
 // initialize checkbox IDs and localStorage records
 if(pastSession) {
     let boxCount = 1;
@@ -35,12 +57,16 @@ if(pastSession) {
     });
 }
 
+// clear localStorage data and refresh page (to re-initialize 'boxes') when reset-btn clicked
 resetBtn.addEventListener('click', () => {
     if(confirm("Are you sure you want to delete your data?")) {
         localStorage.clear();
+        sessionStorage.clear();
         location.reload();
     }
 });
+
+
 
 
 
